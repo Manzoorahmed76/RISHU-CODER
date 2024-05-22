5f8c8cd31e900c6216230b448af64dee90aa8c73
Don't open it <160379580+Manzoorahmed76@users.noreply.github.com>
2024-05-22 03:31:24 -0700

    Update nav_and_links.svelte

--- src/lib/nav_and_links.svelte
+++ src/lib/nav_and_links.svelte
@@ -6,18 +6,18 @@
   ]
 
   export const nav_list = [
-    {text: "Home", link:"https://t.me/synaxnetwork", icon: "fa-house"},
-    {text: "About", link:"https://t.me/a4bhi", icon: "fa-address-card"},
-    {text: "Portfolio", link:"https://t.me/synaxnetwork", icon: "fa-image"},
-    {text: "Contact", link:"https://t.me/verifiedsarthak", icon: "fa-address-book"},
+    {text: "Home", link:"https://t.me/UR_RISHU_143", icon: "fa-house"},
+    {text: "About", link:"https://t.me/Rishu1286", icon: "fa-address-card"},
+    {text: "Portfolio", link:"https://t.me/UR_RISHU_143", icon: "fa-image"},
+    {text: "Contact", link:"https://t.me/Rishu1286", icon: "fa-address-book"},
   ]
 
   export const links = [
-    {name:"Instagram", icon:"fa-brands fa-instagram", color:"text-[--theme-purple]", link: "https://instagram.com/sanatanisynax"},
-    {name:"X", icon:"fa-brands fa-x-twitter", color:"text-[--theme-cyan]", link: "https://twitter.com/SynaxBots"},
-    {name:"Github", icon:"fa-brands fa-github", color:"text-[--theme-white]", link: "https://github.com/SynaxBots"},
-    {name:"Pixiv", icon:"fa-brands fa-pixiv", color:"text-[--theme-blue]", link: "https://t.me/synaxnetwork"},
-    {name:"Youtube", icon:"fa-brands fa-square-youtube", color:"text-[--theme-red]", link: "https://youtube.com/@synaxbots?si=pV-yBM4fDqtpgEWi"},
-    {name:"Support", icon:"fa-brands fa-patreon", color:"text-[--theme-yellow]", link: "https://t.me/synaxnetwork"}
+    {name:"Instagram", icon:"fa-brands fa-instagram", color:"text-[--theme-purple]", link: "https://www.instagram.com/rishabhopi?igsh=c3RjaHh0ZXp6cnNu"},
+    {name:"X", icon:"fa-brands fa-x-twitter", color:"text-[--theme-cyan]", link: "https://t.me/UR_RISHU_143"},
+    {name:"Github", icon:"fa-brands fa-github", color:"text-[--theme-white]", link: "https://github.com/Rishabhopi"},
+    {name:"Pixiv", icon:"fa-brands fa-pixiv", color:"text-[--theme-blue]", link: "https://t.me/UR_RISHU_143"},
+    {name:"Youtube", icon:"fa-brands fa-square-youtube", color:"text-[--theme-red]", link: "https://t.me/UR_RISHU_143"},
+    {name:"Support", icon:"fa-brands fa-patreon", color:"text-[--theme-yellow]", link: "https://t.me/UR_RISHU_143"}
   ]
 </script>
